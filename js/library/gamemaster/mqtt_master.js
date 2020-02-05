3f2dd3ae9a20cb73885d47bc0c2a8364032d8618
swann brunet <swannbrunet@hotmail.fr>
2020-02-05 11:56:13 -0800

    add game Master ( not working just file)

---
+++ js/library/gamemaster/mqtt_master.js
@@ -0,0 +1,9 @@
+//var client = mqtt.connect(broker, options);
+
+function master_help(){
+    console.log('Fonction disponible : \n' +
+        'reset() => réinitialise la map \n' +
+        'teleport() => téléport l\'agent \n' +
+        '')
+}
+
