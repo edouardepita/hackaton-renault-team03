79453bbb8d5cffb17a16fdd5538f91f2321dd9a9
swann brunet <swannbrunet@hotmail.fr>
2020-02-05 07:54:11 -0800

    add mqtt var

---
+++ js/environement.js
@@ -0,0 +1,6 @@
+ //======= MQTT VAR ======
+var BROKER_DOMAIN='mr1dns3dpz5mjj.messaging.solace.cloud';
+var BROKER_USER = 'team03';
+var BROKER_PASSWORD = '7oiag9uv2c';
+var BROKER_MQTT_HOST_PORT = '8443';
+var BROKER_TCP_MQTTHOST_PORT = '1883';
