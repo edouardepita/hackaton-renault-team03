bd3be1d8b5cdee861283883c085adb1a528fbd24
swann brunet <swannbrunet@hotmail.fr>
2020-02-05 09:13:51 -0800

    add global var for password and user

--- js/library/mqtt_controler/mqtt_controler.js
+++ js/library/mqtt_controler/mqtt_controler.js
@@ -1,6 +1,6 @@
 var clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
 
-var broker = BROKER_MQTT_HOST
+var broker = BROKER_MQTT_HOST;
 
 var options = {
   keepalive: 10,
@@ -16,31 +16,31 @@ var options = {
     qos: 0,
     retain: false
   },
-  username: 'team03',
-  password: '7oiag9uv2c',
+  username: BROKER_USER,
+  password: BROKER_PASSWORD,
   rejectUnauthorized: false
-}
+};
 
-var client = mqtt.connect(broker, options)
+var client = mqtt.connect(broker, options);
 
 
 client.on('error', function (err) {
-  console.log(err)
+  console.log(err);
   client.end()
-})
+});
 
 client.on('connect', function () {
   console.log('client connected:' + clientId)
-})
+});
 
-client.subscribe('team03/myteam/test', { qos: 0 })
+client.subscribe('team03/myteam/test', { qos: 0 });
 
-client.publish('team03/myteam/test', 'wss secure connection demo...!', { qos: 0, retain: false })
+client.publish('team03/myteam/test', 'wss secure connection demo...!', { qos: 0, retain: false });
 
 client.on('message', function (topic, message, packet) {
   console.log('Received Message:= ' + message.toString() + '\nOn topic:= ' + topic)
-})
+});
 
 client.on('close', function () {
   console.log(clientId + ' disconnected')
-})
+});
